2ba9524089f0d3d0e342e4aa7237588e899a9865
Georgi Angelov <gtangelov76@gmail.com>
2026-02-02 18:53:47 +0200

    Update car_race.py

--- car_race.py
+++ car_race.py
@@ -1,12 +1,21 @@
-from math import ceil
+from math import floor
 sequence_of_numbers = input().split()
-
+my_list = []
 time_of_first_car = 0.00
 time_of_second_car = 0.00
-idx_of_middle = ceil(len(sequence_of_numbers) / 2)
-sequence_of_numbers.pop(idx_of_middle)
+idx_of_middle = floor(len(sequence_of_numbers) / 2)
 
 for idx in sequence_of_numbers:
     integer_num = int(idx)
+    my_list.append(integer_num)
 
-print(sequence_of_numbers)
+for num in my_list:
+    time_of_first_car += num
+    if num == 0:
+        time_of_first_car *= 0.80
+    if num[5]:
+        reverse_list = my_list[::-1]
+        continue
+
+my_list.pop(idx_of_middle)
+print(my_list)
