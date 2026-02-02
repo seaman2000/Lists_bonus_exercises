d27dc082b309fb8164024c15a1a3bc295dd99270
Georgi Angelov <gtangelov76@gmail.com>
2026-02-02 19:20:47 +0200

    fixed task and created v2

--- car_race.py
+++ car_race.py
@@ -1,21 +1,29 @@
-from math import floor
 sequence_of_numbers = input().split()
+
 my_list = []
-time_of_first_car = 0.00
-time_of_second_car = 0.00
-idx_of_middle = floor(len(sequence_of_numbers) / 2)
+left_car = 0.00
+right_car = 0.00
+idx_of_middle = len(sequence_of_numbers) // 2
 
 for idx in sequence_of_numbers:
     integer_num = int(idx)
     my_list.append(integer_num)
 
-for num in my_list:
-    time_of_first_car += num
-    if num == 0:
-        time_of_first_car *= 0.80
-    if num[5]:
-        reverse_list = my_list[::-1]
-        continue
+for idx, number in enumerate(my_list):
+    if idx < idx_of_middle:
+        left_car += number
+        if number == 0:
+            left_car *= 0.80
 
-my_list.pop(idx_of_middle)
-print(my_list)
+my_list = my_list[::-1]
+
+for idx, number in enumerate(my_list):
+    if idx < idx_of_middle:
+        right_car += number
+        if number == 0:
+            right_car *= 0.80
+
+if left_car < right_car:
+    print(f"The winner is left with total time: {left_car:.1f}")
+elif right_car < left_car:
+    print(f"The winner is right with total time: {right_car:.1f}")
